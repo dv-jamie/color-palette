37b0b3ec3e268763cd0ae5fa37362100257b33d6
dv-jamie <sjm2506@gmail.com>
2022-09-24 23:27:15 +0900

    랜덤 추출 컬러리스트 추가

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,13 +1,57 @@
+import { useEffect, useState } from "react";
 import "./ColorList.css";
 
 function ColorList() {
+    const [colorList, setColorList] = useState([])
+
+    useEffect(() => {
+        const colors = []
+        let i = 0
+        while(i < 5) {
+            i++
+            colors.push(getRandomColor())
+            console.log(i, colors)
+        }
+        setColorList(colors)
+    }, [])
+
+    const getRandomColor = () => {
+        const randomInt = Math.floor(Math.random() * 16777215)
+        return "#" + randomInt.toString(16)
+    }
+
     return (
-        <div class="color-list-wrap">
-            <div class="color-list">1</div>
-            <div class="color-list">2</div>
-            <div class="color-list">3</div>
-            <div class="color-list">4</div>
-            <div class="color-list">5</div>
+        <div className="color-list-wrap">
+            <div
+                className="color-list"
+                style={{ backgroundColor: colorList[0] }}
+            >
+                <span className="color-code">{colorList[0]}</span>
+            </div>
+            <div
+                className="color-list"
+                style={{ backgroundColor: colorList[1] }}
+            >
+                <span className="color-code">{colorList[1]}</span>
+            </div>
+            <div
+                className="color-list"
+                style={{ backgroundColor: colorList[2] }}
+            >
+                <span className="color-code">{colorList[2]}</span>
+            </div>
+            <div
+                className="color-list"
+                style={{ backgroundColor: colorList[3] }}
+            >
+                <span className="color-code">{colorList[3]}</span>
+            </div>
+            <div
+                className="color-list"
+                style={{ backgroundColor: colorList[4] }}
+            >
+                <span className="color-code">{colorList[4]}</span>
+            </div>
         </div>
     );
   }
